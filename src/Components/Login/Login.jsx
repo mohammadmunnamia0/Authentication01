f32a7554ebcb75aff54d5da1808bfa2d1f6f954b
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2024-12-16 11:42:45 +0600

    Authentication

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,35 +1,45 @@
 import { getAuth, signInWithPopup } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth";
+import { useState } from "react";
 import app from "../../firebase/firebase.init";
 
 const Login = () => {
+  const [user, setUser] = useState();
+
   const auth = getAuth(app);
   console.log(app);
   const provider = new GoogleAuthProvider();
 
   const handleGOogleSignIn = () => {
-
     signInWithPopup(auth, provider)
+      .then((result) => {
+        const LogUser = result.user;
+        console.log(LogUser);
+        setUser(LogUser);
+      })
+      .catch((error) => {
+        console.log("error", error.message);
+      });
 
-    .then(result => {
-      const user = result.user;
-      console.log(user);
-    })
-    .catch(error =>{
-      console.log('error', error.message);
-    })
-    
     // console.log("Google Authentication will come soon");
   };
 
   return (
     <div>
-      <button
-        className="flex items-center justify-center btn mt-5"
-        onClick={handleGOogleSignIn}
-      >
-        Login
+      <button className="flex items-center justify-center btn mt-5"
+        onClick={handleGOogleSignIn}>
+
+        Sign In
+
       </button>
+      {user && 
+        <div>
+          <h3> Name : {user.displayName}</h3>
+          <h3> Email : {user.email}</h3>
+        </div>
+      }
+
+    
     </div>
   );
 };
